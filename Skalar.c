3148eaf775f83ddad2fb84cfef614adc64397db6
G-Sell-09 <robinho1808@web.de>
2021-04-19 15:38:39 +0200

    Skalar.c

--- Skalar.c
+++ Skalar.c
@@ -1,1 +1,34 @@
 // Hier wird jetzt programmiert
+
+#include <stdio.h>
+
+// Funktion zur Berechnung des Skalarprodukts
+int dot(int x[], int y[], int dim) {
+  int vproduct = 0;
+  for(int i=0;i<dim;i++){
+    product = vproduct + x[i] * y[i];
+  }
+  return vproduct;
+}
+
+// Hauptroutine
+//int main(int argc, char **argv[]) {
+int main(void) {
+
+  // int dim = atoi(argv[1]);
+  int dim = 2;
+
+  int *x = malloc(dim * sizeof(int));
+  x[0] = 2;
+  x[1] = 3;
+
+  int *y = malloc(dim * sizeof(int));
+  y[0] = 5;
+  y[1] = 2;
+
+  lsg = dot(x,y,dim);
+
+  printf("Das Skalarprodukt beträgt: %s\n", lsg);
+
+  return 0;
+}
