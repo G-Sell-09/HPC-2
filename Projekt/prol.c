20020a5a83783c5b387b5dbc47a8d0eba70dc58a
G-Sell-09 <robinho1808@web.de>
2021-06-03 14:31:36 +0200

    Prolongieren final

--- Projekt/prol.c
+++ Projekt/prol.c
@@ -18,28 +18,19 @@ void prol(int N, double* v_2h, double* v_h)
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       // v_h[2i,2j] = v_2h[i,j]
-      v_h[(NN + 2*i*NN +1) +(2*j)] = v_2h[i*N+j];
+      v_h[(NN + 2*i*NN +1) + (2*j)] = v_2h[i*N+j];
     }
   }
 
-  for (int i=0;i<NN;i++)
-  {
-    for (int j=0;j<NN;j++){
-        printf(" %f ", v_h[i * NN + j]);
-      }
-  printf("\n");
-  }
-  printf("\n");printf("\n");
-
-  // Im Skript falsch farbig markiert. Hier sind die Punkte die immer über und unter den alten Punkten liegen
+  // Hier sind die Punkte die immer über und unter den alten Punkten liegen
   for (int i = 1; i < N; i++) {
-    for (int j = 1; j < N; j++) {
+    for (int j = 0; j < N; j++) {
       //v_h[2i+1,2j] = 1/2 * (v_2h[i,j]+v_2h[i+1,j])
-      v_h[(2*(i+1)*NN) + (2*j)] = 0.5 * (v_2h[i*N+j] + v_2h[(i+1)*N+j]);
+      v_h[((2*i*NN)+1) + (2*j)] = 0.5 * (v_2h[(i-1)*N+j] + v_2h[i*N+j]);
     }
   }
 
-  // Im Skript falsch farbig markiert. Hier sind die Punkte die immer links und rechts neben den alten Punkten liegen
+  // Hier sind die Punkte die immer links und rechts neben den alten Punkten liegen
   for (int i = 0; i < N; i++) {
     for (int j = 1; j < N; j++) {
       // v_h[2i,2j+1] = 1/2 * (v_2h[i,j]+v_2h[i,j+1])
@@ -51,7 +42,8 @@ void prol(int N, double* v_2h, double* v_h)
   for (int i = 1; i < N; i++) {
     for (int j = 1; j < N; j++) {
       // v_h[2i+1,2j+1] = 1/4 * (v_2h[i,j] + v_2h[i,j+1] + v_2h[i+1,j] + v_2h[i+1,j+1])
-      v_h[(2*(i+1)*NN) + (2*j+NN)] = 0.25 * (v_2h[i*N+j] + v_2h[(i+1)*N+j] + v_2h[i*N+(j+1)] + v_2h[(i+1)*N+(j+1)]);
+      v_h[(2*i*NN) + (2*j)] = 0.25 * (v_2h[(i-1)*N+(j-1)] + v_2h[(i-1)*N+(j)] + v_2h[(i)*N+(j-1)] + v_2h[(i)*N+(j)]);
+      printf("Werte für ueberall: %d %d %d %d \n", (i-1)*N+(j-1), (i)*N+(j-1), (i-1)*N+(j), (i)*N+(j));
     }
   }
 
@@ -99,29 +91,17 @@ void prol(int N, double* v_2h, double* v_h)
 void main (int argc , char **argv)
 {
   int N=3;
+  int NNN = 2*N+1;
 
   double *v_2h = (double*) malloc (N*N*sizeof(double));
-  double *v_h = (double*) malloc (N*N*sizeof(double));
+  double *v_h = (double*) malloc (NNN*NNN*sizeof(double));
   for (int i = 0; i < N*N; i++)
   {
     v_2h[i] = i+1;
     v_h[i] = 0;
   }
 
-  for (int i=0;i<N;i++)
-  {
-    for (int j=0;j<N;j++){
-        printf(" %f ", v_2h[i * N + j]);
-      }
-  printf("\n");
-  }
-
-  printf("\n");printf("\n");
-
-  printf(" Fehleröser ");
   prol(N,v_2h,v_h);
-
-  int NNN = 2*N+1;
 
   for (int i=0;i<NNN;i++)
   {
@@ -130,4 +110,5 @@ void main (int argc , char **argv)
       }
   printf("\n");
   }
+
 }
